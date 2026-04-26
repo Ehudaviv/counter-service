c37380f9913c8f5d88adb2eed0981274d0f9800e
Ehudaviv <ehudaviv@outlook.com>
2026-04-27 01:17:46 +0700

    changed to a relevant ami

--- terraform/infra/nodegroup.tf
+++ terraform/infra/nodegroup.tf
@@ -2,7 +2,6 @@ resource "aws_eks_node_group" "system_nodes" {
   cluster_name    = aws_eks_cluster.main.name
   node_group_name = "system-node-group"
   
-  # We are reusing the node IAM role we already cleverly built in karpenter.tf!
   node_role_arn   = aws_iam_role.karpenter_node.arn 
   subnet_ids      = module.vpc.private_subnets
 
@@ -12,9 +11,9 @@ resource "aws_eks_node_group" "system_nodes" {
     min_size     = 2
   }
 
-  # Cost-effective, high-performance Graviton nodes
   instance_types = ["t4g.medium"]
-  ami_type       = "AL2_ARM_64"
+  # Updated to Amazon Linux 2023 to support EKS 1.34
+  ami_type       = "AL2023_ARM_64_STANDARD"
 
   depends_on = [
     aws_iam_role_policy_attachment.karpenter_node_policies
